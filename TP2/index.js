a109e1101121502762ab2b337d63060680aa0478
estebanmaxposse <estebanposse98@gmail.com>
2022-09-08 22:38:11 -0300

    TP2 Manejo de Archivos

---
+++ TP2/index.js
@@ -0,0 +1,9 @@
+const Container = require('./class');
+const data = require('./database');
+
+const products = new Container('./products.txt');
+products.save(data);
+products.getById(2);
+products.getAll();
+products.deleteById(2);
+products.deleteAll();
